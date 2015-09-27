122999a65e1fb0ef06cd93a0d210e424bd6483be
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-09-27 11:50:46 +0000

    Wrote unique_id test, method and implemented in view

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do 
+      quote = Quote.create(:author => 'Cooper', :saying => 'Mankind was born on Earth. It was never meant to die here.')
+      expected = 'C#' + quote.id.to_s
+      actual = quote.unique_tag
+      assert_equal expected, actual
   end
 end
