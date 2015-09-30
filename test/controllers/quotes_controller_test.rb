b8ec311b47e4238395c34ae87c8048804e907969
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-09-30 08:57:14 +0000

    Added unique id to home page, added permalink for each quote, developed tests for the model and controller

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -11,4 +11,17 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'OMG'
     assert_response :not_found
   end
+
+  test "quote about page" do
+    get :about
+    assert_response :success
+  end
+  
+  test "create valid quotee" do
+    assert_difference('Quote.count') do
+        post :create, :quote => { :author => "Cooper", :saying => "No time for caution" }
+    end
+    
+    assert_redirected_to root_path
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -7,4 +7,14 @@ class QuoteTest < ActiveSupport::TestCase
       actual = quote.unique_tag
       assert_equal expected, actual
   end
+
+  test "quote validates" do
+      quote = FactoryGirl.create(:quote)
+      assert quote.valid?
+  end
+  
+  test "invalid quote doesn't validate" do
+    quote = FactoryGirl.build(:quote, author: "")
+    assert_not quote.valid?
+  end
 end
